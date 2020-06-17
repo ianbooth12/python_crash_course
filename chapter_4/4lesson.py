cb1e763f350a81952303c02f6f79eaab464dab8a
ianbooth <off.ibooth@gmail.com>
2020-06-17 12:20:46 -0700

    Lesson content for Chapter 4: Working with Lists

--- chapter_4/4lesson.py
+++ chapter_4/4lesson.py
@@ -20,4 +20,19 @@ squares = []
 for value in range(1,11):
 	square = value **2
 	squares.append(square)  # This loop displays the squares of each value 1-10 by squaring the value within the loop and then appending it.
-print(squares)
+print(squares)  # The loop can be simplified by having one line within the loop, squares.append(value**2)
+
+integers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # Quick Commands for Data Lists
+min(integers)  
+max(integers)
+sum(integers)  # These commands, in a terminal command center, will yield different information about a numerical data list.
+
+cubes = [value**3 for value in range(1,11,1)]  # List Comprehensions
+print(cubes)  # By setting a rule within the list brackets, the results will automatically be appended to the list.
+
+players = ["john", "tanner", "hadi", "kevin", "lucas"]  # Slicing a List
+print(players[0:3])  # Because of Python's "stopping one short" rule, this will print terms 0, 1, and 2.
+print(players[2:6])  # In order to include the last person in the list, use a number one higher than the last in the list.
+print(players[:4])  
+print(players[3:])  # Without a starting or ending parameter, the slice will start at the beginning or end of the list.
+

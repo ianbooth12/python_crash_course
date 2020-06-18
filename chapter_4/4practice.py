10707513d5f22c5c4c6842b7b31409d39930183e
ianbooth <off.ibooth@gmail.com>
2020-06-18 16:08:39 -0700

    Chapter 4 Practice Sections

--- chapter_4/4practice.py
+++ chapter_4/4practice.py
@@ -6,16 +6,16 @@ print("I really love pizza!")  # Pizzas
 animals = ["cat", "dog", "hamster"]
 for animal in animals:
 	print(f"I bet that a {animal} would make a great pet!")
-print("Now that I think about it, any of these pets would be great!")  # Animals
+print("Now that I think about it, any of these pets are great!")  # Animals
 
 numbers = list(range(1,21))  # Counting to Twenty
 for value in numbers:
 	print(value)
 
 digits = [value for value in range(1,21)]
-print(digits)  # These are two different methods to display digits 1 through 20, the first being separated, and the second in an inclusive list.
+print(digits)  # Comprehensive lists can save lines of code
 
-million = [value for value in range(1,1000001)] # The goal here is to print every value from 1 to 1,000,000, it's better to list comprehend this.
+million = [value for value in range(1,1000001)]  # Comprehend larger lists
 
 odds = list(range(1,21,2))  # Odd Numbers
 
@@ -30,5 +30,41 @@ for value in range(1,11):
 print(cubes)
 
 cubec = [value**3 for value in range(1,11)]  # Cube Comprehension
-print(cubec)  # Comprehension can save many lines of code, so it's always better to use if you can understand it.
+print(cubec)  # Comprehension can save many lines of code, use if comfortable
 
+animals.append("tiger")
+animals.append("horse")
+print(animals)
+
+print("The first animals I would think to adopt are:")  # Slices
+print(animals[:3])
+print("Three animals from the middle of my shortlist are:")
+print(animals[1:4])
+print(animals[2:])
+
+friend_pizzas = pizzas[:]  # My pizzas, Your pizzas
+friend_pizzas.append("sausage")
+print("My favorite pizzas are:")
+for pizza in pizzas:
+	print(pizza)
+print("My friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+	print(pizza)
+
+my_foods = ["pizza", "chicken", "ice cream"]  # More Loops
+friend_foods = my_foods[:]
+my_foods.append("pasta")
+friend_foods.append("popsicles")
+print("My favorite foods are")
+for food in my_foods:
+	print(food)
+print("My friend's favorite foods are:")
+for food in friend_foods:
+	print(food)
+
+buffet_foods = ("wings", "chicken", "pizza", "cake", "bagels")  # Tuples
+for food in buffet_foods:
+	print(food)
+buffet_foods = ("wings", "turkey", "pizza", "ice cream", "toast")
+for food in buffet_foods:
+	print(food)

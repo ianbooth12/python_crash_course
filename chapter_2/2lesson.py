71129edf729de9d6d9a38995cd7c9d89c5e3017e
ianbooth <off.ibooth@gmail.com>
2020-06-18 16:15:27 -0700

    Chapter 2: Variables & Simple Data Types

--- chapter_2/2lesson.py
+++ chapter_2/2lesson.py
@@ -1,8 +1,8 @@
 inches_in_foot = 12_000_000
-print(inches_in_foot)  # Although Python removes underscores when printing, using them helps make code more readable
+print(inches_in_foot)  # Although Python removes underscores in print, makes more readable.
 x, y, z = 0, 0 ,0
-print(x)  # You can define multiple variables in one line of code by separating with commas
+print(x)  # You can define multiple variables in one line of code with commas
 name1, name2, name3 = "mason", "jad", "kadin"
 print(name2)  # Multiple variables can also be defined with different information
 
-BIRTHYEAR = 2001  # Variables which should be kept constant should be typed in all caps
+BIRTHYEAR = 2001  # Constant variables can be indicated by typing in caps.

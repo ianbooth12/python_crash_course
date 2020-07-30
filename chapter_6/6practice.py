626972dfa31a2fe256e27318e6d4d89da97a0444
ianbooth <off.ibooth@gmail.com>
2020-07-30 15:36:38 -0700

    Chapter 6 Practice Sections

--- chapter_6/6practice.py
+++ chapter_6/6practice.py
@@ -1,1 +1,19 @@
-kadin = {'first_name': 'kadin', 'last_name': 'rabo', 'age': 18}
+kadin = {  # "Person"
+'first_name': 'kadin',
+ 'last_name': 'rabo',
+  'age': 18,
+   'college': 'cal_poly'
+   }
+
+favorite_number = {  # Favorite Numbers
+	'ian': 12,
+	'kadin': 11,
+	'jad': 1,
+	'nicolas': 5,
+	'mason': 88,
+}
+
+python_terms = {
+	'list_modifiers': '\nCommon modifiers include: \n.pop (removes last term)\
+	\n.append (adds term to the end)\n.insert (Add to a specific position),\
+	

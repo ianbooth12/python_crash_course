19d107b77f0147a529ad0b7fbdbcd4cc93642da9
ianbooth <off.ibooth@gmail.com>
2020-08-03 17:27:03 -0700

    Chapter 6 Practice Sections

--- chapter_6/6practice.py
+++ chapter_6/6practice.py
@@ -21,3 +21,29 @@ python_terms = {  # Glossary
 for key, value in python_terms.items():
 	print(f"\nTerm: {key.title()}")
 	print(f"Definition: {value.title()}")
+
+rivers = {  # Rivers
+	'hudson': 'united states',
+	'nile': 'egypt',
+	'amazon': 'brazil'
+}
+for river, country in rivers.items():
+	print(f"\nThe {river.title()} river flows through {country.title()}.")
+for river in rivers.keys():
+	print(river)
+for country in rivers.values():
+	print(country)
+
+poll_participants = ['mason', 'kavi', 'jad', 'nicolas', 'diego']
+favorite_languages = {  # Polling
+	'kadin': 'python',
+	'jad': 'c++',
+	'mathys': 'java',
+	'kavi': 'c'
+	}
+for participant in poll_participants:
+	if participant in favorite_languages.keys():
+		print(f"Thanks for filling out the survey, {participant.title()}!")
+	else:
+		print(f"Could you please fill the survey, {participant.title()}?")
+
